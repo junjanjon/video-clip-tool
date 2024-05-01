be6c859581fd2c1ca681a6b40fabec197dd04a30
hiroi_junki <junki.hiroi@drecom.co.jp>
2024-05-02 01:30:34 +0900

    Update video progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,25 @@ import MovieIcon from '@mui/icons-material/Movie';
 import Box from '@mui/material/Box';
 import MovieFileSelector from './components/MovieFileSelector.tsx';
 
-function VideoProgressBar(props: { currentTime: number, minTime: number, maxTime: number }) {
+function VideoProgressBar(props: { currentTime: number, minTime: number, maxTime: number, changeCallback: (time: number) => void}) {
   const percent = (props.currentTime - props.minTime) / (props.maxTime - props.minTime);
   return (
     <Box sx={{ display: 'flex', alignItems: 'center' }}>
       <Box sx={{ width: '100%', mr: 1 }}>
-        <progress value={percent} style={{
-          width: '100%',
-        }} />
+        <input
+          type={'range'}
+          value={percent}
+          onChange={(event) => {
+            const value = event.target.value;
+            const time = props.minTime + (props.maxTime - props.minTime) * parseFloat(value);
+            props.changeCallback(time);
+          }}
+          min={0}
+          max={1}
+          step={0.001}
+          style={{
+            width: '100%',
+          }} />
       </Box>
       <Box sx={{ minWidth: 35 }}>
         <div>{convertTimeToText(props.currentTime)}</div>
@@ -113,7 +124,7 @@ function App() {
           setCurrentProgressTime(() => video.currentTime);
         }
       }
-    }, 1 / 60);
+    }, 1000 / 60);
     return () => clearInterval(interval);
   }, [trimTime]);
 
@@ -263,7 +274,9 @@ function App() {
         <VideoProgressBar
           currentTime={currentProgressTime}
           minTime={trimTime[0]}
-          maxTime={trimTime[1]}/>
+          maxTime={trimTime[1]}
+          changeCallback={playVideoWrapper}
+        />
         <Slider
           // 全動画時間の中での位置を指定するスライダー
           value={trimTime[0]}
